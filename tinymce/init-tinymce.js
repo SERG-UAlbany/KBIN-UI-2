88351e6383f82d78bde6f3708c4dd5d1b1556eaa
KhushbooShah <shahkhushboo0@gmail.com>
2018-08-12 12:42:30 -0400

    Commit with final changes

--- tinymce/init-tinymce.js
+++ tinymce/init-tinymce.js
@@ -1,6 +1,9 @@
 tinymce.init({
     selector:"textarea.tinymce",
-
+    branding: false,
+    menubar:false,
+    toolbar:'formatselect | fontsizeselect | bold  italic  underline  backcolor | alignleft  aligncenter  alignright  alignjustify | link image',
     width:"100%",
     height:300
+
 });
